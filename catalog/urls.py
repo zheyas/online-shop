cf99af422f9a2055cbdb0843d49524040ab756a6
asako <asakovevgenij608@gmail.com>
2025-03-09 21:21:41 +0500

    ДЗ

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import HomeView, ContactView, IndexView
-
+from .views import (
+    HomeView, ContactView, IndexView,
+    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView,
+    ProductUpdateView
+)
 
 urlpatterns = [
+    # Основные страницы
     path('', HomeView.as_view(), name='home'),
     path('contacts/', ContactView.as_view(), name='contacts'),
     path('index/', IndexView.as_view(), name='index'),
+
+    # Продукты (CRUD)
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
