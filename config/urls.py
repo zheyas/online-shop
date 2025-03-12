c082d8d75d15c0d2ca5667e9efc2c3939b561d47
asako <asakovevgenij608@gmail.com>
2025-03-12 23:28:37 +0500

    корректировки путей

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# config/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from users.views import UserRegisterView, CustomLoginView, UserProfileUpdateView  # проверьте правильность путей
 
 urlpatterns = [
     path('', include('catalog.urls')),
@@ -29,9 +13,8 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('profile/edit/', UserProfileUpdateView.as_view(), name='edit_profile'),
-
+    path('users/', include('users.urls')),  # Подключите users.urls для пользовательских маршрутов
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/templates/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import UserRegisterView, UserLoginView, UserProfileView, UserLogoutView
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+]
