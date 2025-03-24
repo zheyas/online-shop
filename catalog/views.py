7a6dca0f3ce62dc862ccac8b031550e1381cdd9a
asako <asakovevgenij608@gmail.com>
2025-03-24 18:17:48 +0500

    DZ

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,13 @@
+# catalog/views.py
 
-from django.views.generic import ListView, FormView, TemplateView
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView, FormView
 from .models import Product
 from .forms import ContactForm, ProductForm
-from django.urls import reverse_lazy
 from django.contrib import messages
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin  # Импортируем миксины
+from django.contrib.auth.decorators import login_required
 
 class HomeView(ListView):
     model = Product
@@ -45,8 +46,17 @@ class ProductListView(ListView):
     template_name = "catalog/product_list.html"
     context_object_name = "products"
 
+    def get_queryset(self):
+        """Отображаем продукты текущего пользователя"""
+        return Product.objects.filter(owner=self.request.user)
 
-class ProductCreateView(CreateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.get_queryset()
+        return context
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):  # Добавляем LoginRequiredMixin
     """Создание продукта"""
     model = Product
     form_class = ProductForm
@@ -54,6 +64,7 @@ class ProductCreateView(CreateView):
     success_url = reverse_lazy("product_list")
 
     def form_valid(self, form):
+        form.instance.owner = self.request.user  # Автоматическое назначение владельца
         messages.success(self.request, "Продукт успешно создан.")
         return super().form_valid(form)
 
@@ -61,8 +72,13 @@ class ProductCreateView(CreateView):
         messages.error(self.request, "Ошибка при создании продукта. Пожалуйста, проверьте форму на наличие ошибок.")
         return self.render_to_response(self.get_context_data(form=form))
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Создание продукта'  # Example: For header in template
+        return context
 
-class ProductUpdateView(UpdateView):
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """Редактирование продукта"""
     model = Product
     form_class = ProductForm
@@ -80,13 +96,47 @@ class ProductUpdateView(UpdateView):
     def get_object(self, queryset=None):
         return get_object_or_404(Product, pk=self.kwargs['pk'])
 
+    def test_func(self):
+        """Проверка, является ли пользователь владельцем"""
+        obj = self.get_object()
+        return obj.owner == self.request.user
 
-class ProductDeleteView(DeleteView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактирование продукта'  # Example: For header in template
+        return context
+
+
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """Удаление продукта"""
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy("product_list")
 
+    def test_func(self):
+        """Проверка, является ли пользователь владельцем или модератором"""
+        obj = self.get_object()
+        return obj.owner == self.request.user or self.request.user.has_perm('catalog.can_unpublish_product')  # Добавлена проверка на права модератора
+
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, "Продукт успешно удалён.")
         return super().delete(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Удаление продукта'  # Example: For header in template
+        return context
+
+@login_required
+def toggle_publish(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    # Проверка разрешения на снятие с публикации
+    if request.user.has_perm('catalog.can_unpublish_product'):
+        product.is_published = not product.is_published
+        product.save()
+        messages.success(request, f"Статус публикации продукта '{product.name}' изменен.")
+    else:
+        messages.error(request, "У вас нет прав на изменение статуса публикации продукта.")
+
+    return redirect('product_list')  # Или куда нужно перенаправить
