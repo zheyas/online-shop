64799ab20c4023676d242aa67c5de8b51316a5d7
asako <asakovevgenij608@gmail.com>
2025-03-28 21:47:32 +0500

    Супер классный комментарий к коммиту

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,8 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+REDIS_URL = os.getenv('REDIS_URL')
 
 
 CACHES = {
